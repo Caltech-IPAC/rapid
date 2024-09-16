3eace0ac0d2a5b3b239111276cf3a06e095a3dc7
Russ Laher <laher@ipac.caltech.edu>
2024-09-16 13:45:17 -0700

    Added ppid=15 science pipeline.

---
+++ database/schema/rapidOpsPipelinesInserts.sql
@@ -0,0 +1,9 @@
+--------------------------------------------------------------------------------------------------------------------------
+-- rapidOpsPipelinesInserts
+--
+-- Russ Laher (laher@ipac.caltech.edu)
+--
+-- 16 September 2024
+--------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO pipelines (ppid,priority,script,descrip) values (15,3,'awsBatchSubmitJobs_launchSingleSciencePipeline.py', 'Science pipeline for input SCA image.');
