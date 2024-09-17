23c83dd7f1dd99c9d7d39b407f7fe8a6121f395d
Russ Laher <laher@ipac.caltech.edu>
2024-09-17 05:13:09 -0700

    Added standard reference-image pipeline.

--- database/schema/rapidOpsPipelinesInserts.sql
+++ database/schema/rapidOpsPipelinesInserts.sql
@@ -7,3 +7,4 @@
 --------------------------------------------------------------------------------------------------------------------------
 
 INSERT INTO pipelines (ppid,priority,script,descrip) values (15,3,'awsBatchSubmitJobs_launchSingleSciencePipeline.py', 'Science pipeline for input SCA image.');
+INSERT INTO pipelines (ppid,priority,script,descrip) values (12,4,'generateReferenceImage.py', 'Standard reference-image pipeline.');
