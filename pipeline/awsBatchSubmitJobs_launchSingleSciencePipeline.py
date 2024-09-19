5a84bf0a1617ac6c0e75adb1c0674ea45ee128d3
Russ Laher <laher@ipac.caltech.edu>
2024-09-19 07:26:27 -0700

    Added more lines of code.

--- pipeline/awsBatchSubmitJobs_launchSingleSciencePipeline.py
+++ pipeline/awsBatchSubmitJobs_launchSingleSciencePipeline.py
@@ -11,12 +11,11 @@ import database.modules.utils.roman_tessellation_db as sqlite
 
 swname = "awsBatchSubmitJobs_launchSingleSciencePipeline.py"
 swvers = "1.0"
+cfg_filename_only = "awsBatchSubmitJobs_launchSingleSciencePipeline.ini"
 
-ppid = 15
-ppid_refimage = 12
 
-    print("swname =", swname)
-    print("swvers =", swvers)
+print("swname =", swname)
+print("swvers =", swvers)
 
 
 # RID of input file.
@@ -63,6 +62,32 @@ if aws_secret_access_key is None:
 print("aws_access_key_id =",aws_access_key_id)
 print("aws_secret_access_key =",aws_secret_access_key)
 
+rapid_sw = os.getenv('RAPID_SW')
+
+if rapid_sw is None:
+
+    print("*** Error: Env. var. RAPID_SW not set; quitting)
+    exit(64)
+
+print("rapid_sw =",rapid_sw)
+
+
+# Read input parameters from .ini file.
+
+config_input_filename = cfg_path + "/" + cfg_filename_only
+config_input = configparser.ConfigParser()
+config_input.read(config_input_filename)
+
+verbose = int(config_input['DEFAULT']['verbose'])
+debug = int(config_input['DEFAULT']['debug'])
+ppid = int(config_input['SCI_IMAGE']['ppid'])
+ppid_refimage = int(config_input['REF_IMAGE']['ppid_refimage'])
+naxis1_refimage = int(config_input['REF_IMAGE']['naxis1_refimage'])
+naxis2_refimage = int(config_input['REF_IMAGE']['naxis2_refimage'])
+cdelt1_refimage = float(config_input['REF_IMAGE']['cdelt1_refimage'])
+cdelt2_refimage = float(config_input['REF_IMAGE']['cdelt2_refimage'])
+crota2_refimage = float(config_input['REF_IMAGE']['crota2_refimage'])
+
 
 # Set up AWS Batch.
 
@@ -71,17 +96,19 @@ client = boto3.client('batch')
 
 # Define job definition.  Use AWS Batch Console to set this up once.
 
-job_definition = "arn:aws:batch:us-west-2:891377127831:job-definition/Fetch_and_run:2"
+job_definition = config_input['AWS_BATCH']['job_definition']
 
 
 # Define job queue.  Use AWS Batch Console to set this up once.
 
-job_queue = 'arn:aws:batch:us-west-2:891377127831:job-queue/getting-started-wizard-job-queue'
+job_queue = config_input['AWS_BATCH']['job_queue']
 
 
 # Define job name.
 
-job_name_base = "rapid_science_pipeline"
+job_name_base = config_input['AWS_BATCH']['job_name_base']
+
+
 
 
 
@@ -213,15 +240,23 @@ if __name__ == '__main__':
 
     dbh = db.RAPIDDB()
 
+    if dbh.exit_code >= 64:
+        exit(dbh.exit_code)
 
     # Query database for associated L2FileMeta record.
 
     sca,fid,ra0,dec0,ra1,dec1,ra2,dec2,ra3,dec3,ra4,dec4 = dbh.get_l2filemeta_record(rid)
 
+    if dbh.exit_code >= 64:
+        exit(dbh.exit_code)
+
 
     # Query database for select columns in L2Files record.
 
     image_info = dbh.get_info_for_l2file(rid)
+
+    if dbh.exit_code >= 64:
+        exit(dbh.exit_code)
 
     filename = image_info[0]
     expid = image_info[1]=
@@ -231,6 +266,12 @@ if __name__ == '__main__':
     exptime = image_info[5]
     infobits = image_info[6]
     status = image_info[7]
+    vbest = image_info[8]
+    version = image_info[9]
+
+    if vbest == 0:
+        print('*** Error: vbest is zero for rid = {}; quitting....'.format(rid))
+     exit(64)
 
 
     #
@@ -243,89 +284,177 @@ if __name__ == '__main__':
     rtid = roman_tessellation_db.rtid
 
     if rtid != field:
-         print("*** Error: rtid does not match field; quitting....")
-         exit(64)
+        print("*** Error: rtid (= {}) does not match field (= {}); quitting....".format(rtid,field))
+        exit(64)
 
     roman_tessellation_db.get_center_sky_position(rtid)
-    field_ra0 = roman_tessellation_db.ra0
-    field_dec0 = roman_tessellation_db.dec0
+    ra0_field = roman_tessellation_db.ra0
+    dec0_field = roman_tessellation_db.dec0
     roman_tessellation_db.get_corner_sky_positions(rtid)
-    field_ra1 = roman_tessellation_db.ra1
-    field_dec1 = roman_tessellation_db.dec1
-    field_ra2 = roman_tessellation_db.ra2
-    field_dec2 = roman_tessellation_db.dec2
-    field_ra3 = roman_tessellation_db.ra3
-    field_dec3 = roman_tessellation_db.dec3
-    field_ra4 = roman_tessellation_db.ra4
-    field_dec4 = roman_tessellation_db.dec4
+    ra1_field = roman_tessellation_db.ra1
+    dec1_field = roman_tessellation_db.dec1
+    ra2_field = roman_tessellation_db.ra2
+    dec2_field = roman_tessellation_db.dec2
+    ra3_field = roman_tessellation_db.ra3
+    dec3_field = roman_tessellation_db.dec3
+    ra4_field = roman_tessellation_db.ra4
+    dec4_field = roman_tessellation_db.dec4
 
 
     # Query RefImages database table for the best (latest unless version is locked) version of reference image.
     # A reference image depends only on pipeline number, field, filter, and version.
 
-    rfid,refim_filename = dbh.get_best_reference_image(ppid_refimage,field,fid)
+    rfid,filename_refimage = dbh.get_best_reference_image(ppid_refimage,field,fid)
 
-    if rfid is None:
+    if dbh.exit_code >= 64:
+        exit(dbh.exit_code)
+
+    if rfid is not None:
+        input_images_csv_file = None
+    else:
+        input_images_csv_file = "ref_image_inputs_jid" + str(jid) + ".csv"
 
         # Query L2FileMeta database table for RID,ra0,dec0,ra1,dec1,ra2,dec2,ra3,dec3,ra4,dec4,
         # and distance from tile center (degrees) for all science images that
         # overlap the sky tile associated with the input science image and its filter.
         # Use radius_of_initial_cone_search = 0.18 degrees.
         # Returned list is ordered by distance from tile center.
+        #
+        # NOTE: The returned list includes all versions, and regardless of status (the query of the
+        # L2FileMeta table does NOT join with the L2Files table, in order to optimize query speed).
 
         radius_of_initial_cone_search = 0.18
-        overlapping_images = dbh.get_overlapping_l2files(rid,fid,field_ra0,field_dec0,field_ra1,field_dec1,field_ra2,field_dec2,
-                                                         field_ra3,field_dec3,field_ra4,field_dec4,radius_of_initial_cone_search)
+        overlapping_images = dbh.get_overlapping_l2files(rid,fid,ra0_field,dec0_field,ra1_field,dec1_field,ra2_field,dec2_field,
+                                                         ra3_field,dec3_field,ra4_field,dec4_field,radius_of_initial_cone_search)
+
+        if dbh.exit_code >= 64:
+            exit(dbh.exit_code)
 
 
         # For each overlapping image, query L2Files database table for filename, sca, mjdobs, exptime, infobits, and status.
 
-        refim_inputs_filename_list = []
-        refim_inputs_exid_list = []
-        refim_inputs_sca_list = []
-        refim_inputs_field_list = []
-        refim_inputs_mjdobs_list = []
-        refim_inputs_exptime_list = []
-        refim_inputs_infobits_list = []
-        refim_inputs_status_list = []
+        filename_refimage_inputs = []
+        exid_refimage_inputs = []
+        sca_refimage_inputs = []
+        field_refimage_inputs = []
+        mjdobs_refimage_inputs = []
+        exptime_refimage_inputs = []
+        infobits_refimage_inputs = []
 
         for image in overlapping_images:
-            refim_inputs_rid = image[0]
+            rid_refimage = image[0]
 
-            image_info = dbh.get_info_for_l2file(refim_inputs_rid)
+            image_info = dbh.get_info_for_l2file(rid_refimage)
 
-            refim_inputs_filename = image_info[0]
-            refim_inputs_expid = image_info[1]=
-            refim_inputs_sca = image_info[2]
-            refim_inputs_field = image_info[3]
-            refim_inputs_mjdobs = image_info[4]
-            refim_inputs_exptime = image_info[5]
-            refim_inputs_infobits = image_info[6]
-            refim_inputs_status = image_info[7]
+            if dbh.exit_code >= 64:
+                exit(dbh.exit_code)
 
-            refim_inputs_filename_list.append(refim_inputs_filename)
-            refim_inputs_expid_list.append(refim_inputs_expid)
-            refim_inputs_sca_list.append(refim_inputs_sca)
-            refim_inputs_field_list.append(refim_inputs_field)
-            refim_inputs_mjdobs_list.append(refim_inputs_mjdobs)
-            refim_inputs_exptime_list.append(refim_inputs_exptime)
-            refim_inputs_infobits_list.append(refim_inputs_infobits)
-            refim_inputs_status_list.append(refim_inputs_status)
+            filename_refimage = image_info[0]
+            expid_refimage = image_info[1]=
+            sca_refimage = image_info[2]
+            field_refimage = image_info[3]
+            mjdobs_refimage = image_info[4]
+            exptime_refimage = image_info[5]
+            infobits_refimage = image_info[6]
+            status_refimage = image_info[7]
+            vbest_refimage = image_info[8]
+            version_refimage = image_info[9]
+
+            if status_refimage == 0: continue             # Omit if status = 0
+            if vbest_refimage == 0: continue              # Omit if not the best version
+
+            filename.append(refim_inputs_filename_refimage)
+            expid.append(refim_inputs_expid_refimage)
+            sca.append(refim_inputs_sca_refimage)
+            field.append(refim_inputs_field_refimage)
+            mjdobs.append(refim_inputs_mjdobs_refimage)
+            exptime.append(refim_inputs_exptime_refimage)
+            infobits.append(refim_inputs_infobits_refimage)
 
 
     # Insert or update record in Jobs database table and return job ID.
 
     jid = dbh.start_job(ppid,fid,expid,field,sca,rid)
 
-
+    if dbh.exit_code >= 64:
+        exit(dbh.exit_code)
 
 
     # Close database connection.
 
     dbh.close()
 
+    if dbh.exit_code >= 64:
+        exit(dbh.exit_code)
+
 
     # Write config file for job.
 
+    config_output = configparser.ConfigParser()
 
-    submit_job()
+    config_output['DEFAULT'] = {'debug': str(debug),
+
+                         'swname': swname,
+
+                         'swvers': swvers,
+                         'jid': str(jid)}
+    config_output['DEFAULT']['verbose'] = str(verbose)
+
+    config_output['SCI_IMAGE'] = {}
+
+    config_output['SCI_IMAGE']['ppid'] = str(ppid)
+    config_output['SCI_IMAGE']['rid'] = str(rid)
+    config_output['SCI_IMAGE']['sca'] = str(sca)
+    config_output['SCI_IMAGE']['fid'] = str(fid)
+
+    config_output['SCI_IMAGE']['filename'] = str(filename)
+    config_output['SCI_IMAGE']['expid'] = str(expid)
+    config_output['SCI_IMAGE']['sca'] = str(sca)
+    config_output['SCI_IMAGE']['field'] = str(field)
+    config_output['SCI_IMAGE']['mjdobs'] = str(mjdobs)
+    config_output['SCI_IMAGE']['exptime'] = str(exptime)
+    config_output['SCI_IMAGE']['infobits'] = str(infobits)
+    config_output['SCI_IMAGE']['status'] = str(status)
+
+    config_output['SCI_IMAGE']['ra0'] = str(ra0)
+    config_output['SCI_IMAGE']['dec0'] = str(dec0)
+    config_output['SCI_IMAGE']['ra1'] = str(ra1)
+    config_output['SCI_IMAGE']['dec1'] = str(dec1)
+    config_output['SCI_IMAGE']['ra2'] = str(ra2)
+    config_output['SCI_IMAGE']['dec2'] = str(dec2)
+    config_output['SCI_IMAGE']['ra3'] = str(ra3)
+    config_output['SCI_IMAGE']['dec3'] = str(dec3)
+    config_output['SCI_IMAGE']['ra4'] = str(ra4)
+    config_output['SCI_IMAGE']['dec4'] = str(dec4)
+
+    config_output['SKY_TILE'] = {}
+
+    config_output['SKY_TILE']['rtid'] = str(rtid)
+
+    config_output['SKY_TILE']['ra0'] = str(ra0_field)
+    config_output['SKY_TILE']['dec0'] = str(dec0_field)
+    config_output['SKY_TILE']['ra1'] = str(ra1_field)
+    config_output['SKY_TILE']['dec1'] = str(dec1_field)
+    config_output['SKY_TILE']['ra2'] = str(ra2_field)
+    config_output['SKY_TILE']['dec2'] = str(dec2_field)
+    config_output['SKY_TILE']['ra3'] = str(ra3_field)
+    config_output['SKY_TILE']['dec3'] = str(dec3_field)
+    config_output['SKY_TILE']['ra4'] = str(ra4_field)
+    config_output['SKY_TILE']['dec4'] = str(dec4_field)
+
+
+    config_output['REF_IMAGE'] = {}
+
+    config_output['REF_IMAGE']['ppid'] = str(ppid_refimage)
+    config_output['REF_IMAGE']['naxis1'] = str(naxis1_refimage)
+    config_output['REF_IMAGE']['naxis2'] = str(naxis2_refimage)
+    config_output['REF_IMAGE']['rfid'] = str(rfid)
+    config_output['REF_IMAGE']['filename'] = filename_refimage
+    config_output['REF_IMAGE']['input_images_csv_file'] = input_images_csv_file
+
+    config_output_filename = "job_config_jid" + str(jid) + ".ini"
+    with open(config_output_filename, 'w') as config_outputfile:
+
+    config_output.write(config_outputfile)
+
+    #submit_job()
