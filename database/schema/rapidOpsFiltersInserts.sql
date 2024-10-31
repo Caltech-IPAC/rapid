80b602b7dadb4a6944ea5526dafb71205dd4f573
Russ Laher <laher@ipac.caltech.edu>
2024-10-31 09:07:34 -0700

    Added W146.

--- database/schema/rapidOpsFiltersInserts.sql
+++ database/schema/rapidOpsFiltersInserts.sql
@@ -13,3 +13,4 @@ INSERT INTO filters values (4, 'K213');
 INSERT INTO filters values (5, 'R062');
 INSERT INTO filters values (6, 'Y106');
 INSERT INTO filters values (7, 'Z087');
+INSERT INTO filters values (8, 'W146');
