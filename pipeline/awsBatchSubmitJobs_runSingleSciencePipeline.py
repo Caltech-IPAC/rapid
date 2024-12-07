a228ec837fb950b28a1699f834b29b8a69b6a67e
Russ Laher <laher@ipac.caltech.edu>
2024-12-07 07:10:13 -0800

    Specified sextractor_PARAMETERS_NAME.

--- pipeline/awsBatchSubmitJobs_runSingleSciencePipeline.py
+++ pipeline/awsBatchSubmitJobs_runSingleSciencePipeline.py
@@ -771,6 +771,7 @@ if __name__ == '__main__':
 
 
     sextractor_dict["sextractor_input_image".lower()] = filename_diffimage_masked
+    sextractor_dict["sextractor_PARAMETERS_NAME".lower()] = "/code/cdf/rapidSexParamsDiffImage.inp"
     sextractor_dict["sextractor_CATALOG_NAME".lower()] = filename_diffimage_masked.replace(".fits",".txt")
 
     sextractor_cmd = util.build_sextractor_command_line_args(sextractor_dict)
