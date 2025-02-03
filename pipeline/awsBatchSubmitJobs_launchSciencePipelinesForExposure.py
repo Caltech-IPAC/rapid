4929ac0bf7f00f58d9eb6f3250ffb9ca380a882b
Russ Laher <laher@ipac.caltech.edu>
2025-02-03 09:50:36 -0800

    Initial check-in.

---
+++ pipeline/awsBatchSubmitJobs_launchSciencePipelinesForExposure.py
@@ -0,0 +1,56 @@
+import database.modules.utils.rapid_db as db
+
+swname = "awsBatchSubmitJobs_launchSciencePipelinesForExposure.py"
+swvers = "1.0"
+
+print("swname =", swname)
+print("swvers =", swvers)
+
+
+# EXPID of input file, read from environment variable EXPID.
+
+expid = os.getenv('EXPID')
+
+if expid is None:
+
+    print("*** Error: Env. var. EXPID not set; quitting...")
+    exit(64)
+
+
+if __name__ == '__main__':
+
+
+    #
+    # Launch a science pipelines for input exposure ID.
+    #
+
+
+    # Open database connection.
+
+    dbh = db.RAPIDDB()
+
+    if dbh.exit_code >= 64:
+        exit(dbh.exit_code)
+
+
+    # Query database for all L2Files records associated with the given exposure ID.
+
+    recs = dbh.get_l2files_records_for_expid(expid)
+
+    for rec in recs:
+        rid = rec[0]
+        sca = rec[1]
+        print("rid, sca =",rid,sca)
+
+    if dbh.exit_code >= 64:
+        print("*** Error from {}; quitting ".format(swname))
+        exit(dbh.exit_code)
+
+
+    # Close database connection.
+
+    dbh.close()
+
+    if dbh.exit_code >= 64:
+        exit(dbh.exit_code)
+
