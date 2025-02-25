8bbad487635bb009dcae5715ae6c8f0da5d72d8c
Russ Laher <laher@ipac.caltech.edu>
2025-02-25 07:43:13 -0800

    Switched back to RTD HTML theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,8 +56,8 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
