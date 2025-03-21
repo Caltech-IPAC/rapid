fe4d3572b783a7a586d359b5c10fd8959042a98f
Russ Laher <laher@ipac.caltech.edu>
2025-03-21 07:39:43 -0700

    Python script for new RAPID post-processing pipeline.

---
+++ pipeline/awsBatchSubmitJobs_runSinglePostProcPipeline.py
@@ -0,0 +1,173 @@
+"""
+The purpose of the post-processing pipeline is to add database IDs
+to products already made by the science pipeline.  This is run only
+after the products have been registered in the RAPID operations database.
+"""
+
+
+import boto3
+import os
+import numpy as np
+from datetime import datetime, timezone
+from dateutil import tz
+import time
+
+to_zone = tz.gettz('America/Los_Angeles')
+
+import modules.utils.rapid_pipeline_subs as util
+import database.modules.utils.rapid_db as db
+import pipeline.referenceImageSubs as rfis
+import pipeline.differenceImageSubs as dfis
+
+start_time_benchmark = time.time()
+
+
+swname = "awsBatchSubmitJobs_runSinglePostProcPipeline.py"
+swvers = "1.0"
+
+print("swname =", swname)
+print("swvers =", swvers)
+
+aws_batch_job_id = os.getenv('AWS_BATCH_JOB_ID')
+print("aws_batch_job_id =", aws_batch_job_id)
+
+
+# Compute processing datetime (UT) and processing datetime (Pacific time).
+
+datetime_utc_now = datetime.utcnow()
+proc_utc_datetime = datetime_utc_now.strftime('%Y-%m-%dT%H:%M:%SZ')
+datetime_pt_now = datetime_utc_now.replace(tzinfo=timezone.utc).astimezone(tz=to_zone)
+proc_pt_datetime_started = datetime_pt_now.strftime('%Y-%m-%dT%H:%M:%S PT')
+
+print("proc_utc_datetime =",proc_utc_datetime)
+print("proc_pt_datetime_started =",proc_pt_datetime_started)
+
+
+# JOBPROCDATE of pipeline job.
+
+job_proc_date = os.getenv('JOBPROCDATE')
+
+if job_proc_date is None:
+
+    print("*** Error: Env. var. JOBPROCDATE not set; quitting...")
+    exit(64)
+
+
+# RAPID_JOB_ID of pipeline job.
+
+jid = os.getenv('RAPID_JOB_ID')
+
+if jid is None:
+
+    print("*** Error: Env. var. RAPID_JOB_ID not set; quitting...")
+    exit(64)
+
+
+# JOBS3BUCKET of pipeline job.
+
+job_info_s3_bucket = os.getenv('JOBS3BUCKET')
+
+if job_info_s3_bucket is None:
+
+    print("*** Error: Env. var. JOBS3BUCKET not set; quitting...")
+    exit(64)
+
+
+# JOBCONFIGFILENAME of pipeline job.
+
+job_config_ini_file_filename = os.getenv('JOBCONFIGFILENAME')
+
+if job_config_ini_file_filename is None:
+
+    print("*** Error: Env. var. JOBCONFIGFILENAME not set; quitting...")
+    exit(64)
+
+
+# JOBCONFIGOBJNAME of pipeline job.
+
+job_config_ini_file_s3_bucket_object_name = os.getenv('JOBCONFIGOBJNAME')
+
+if job_config_ini_file_s3_bucket_object_name is None:
+
+    print("*** Error: Env. var. JOBCONFIGOBJNAME not set; quitting...")
+    exit(64)
+
+
+# REFIMAGEINPUTSFILENAME of pipeline job.
+
+input_images_csv_filename = os.getenv('REFIMAGEINPUTSFILENAME')
+
+if input_images_csv_filename is None:
+
+    print("*** Error: Env. var. REFIMAGEINPUTSFILENAME not set; quitting...")
+    exit(64)
+
+
+# REFIMAGEINPUTSOBJNAME of pipeline job.
+
+input_images_csv_file_s3_bucket_object_name = os.getenv('REFIMAGEINPUTSOBJNAME')
+
+if input_images_csv_file_s3_bucket_object_name is None:
+
+    print("*** Error: Env. var. REFIMAGEINPUTSOBJNAME not set; quitting...")
+    exit(64)
+
+
+# Print out basic information for log file.
+
+print("job_proc_date =",job_proc_date)
+print("jid =",jid)
+print("job_info_s3_bucket =",job_info_s3_bucket)
+print("job_config_ini_file_filename =",job_config_ini_file_filename)
+print("job_config_ini_file_s3_bucket_object_name =",job_config_ini_file_s3_bucket_object_name)
+print("input_images_csv_file_s3_bucket_object_name =",input_images_csv_file_s3_bucket_object_name)
+
+
+#-------------------------------------------------------------------------------------------------------------
+# Main program.
+#-------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+
+    # Initialize termination exitcode to normal termination.
+    # If detected during post-processing, an abnormal termination exitcode will be set.
+
+    terminating_exitcode = 0
+
+
+    # Get S3 resource.
+
+    s3_resource = boto3.resource('s3')
+
+
+
+
+    # Inventory products associated with job.
+
+    product_bucket = s3_resource.Bucket(product_s3_bucket_base)
+
+    for product_bucket_object in product_bucket.objects.filter(Prefix=job_prefix):
+
+        print("------==------->",product_bucket_object)
+
+
+
+
+
+
+    # Code-timing benchmark.
+
+    end_time_benchmark = time.time()
+    print("Elapsed time in seconds to launch pipelines =",
+        end_time_benchmark - start_time_benchmark)
+    start_time_benchmark = end_time_benchmark
+
+
+    # Termination.
+
+    print("terminating_exitcode =",terminating_exitcode)
+
+    exit(terminating_exitcode)
+
+
