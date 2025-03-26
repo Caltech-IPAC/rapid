b743f94da9646873979678b04a996b69bb2c8abe
Russ Laher <laher@ipac.caltech.edu>
2025-03-26 08:15:51 -0700

    Added ppid=17 for post-processing pipeline.

--- database/schema/rapidOpsPipelinesInserts.sql
+++ database/schema/rapidOpsPipelinesInserts.sql
@@ -8,3 +8,4 @@
 
 INSERT INTO pipelines (ppid,priority,script,descrip) values (15,3,'awsBatchSubmitJobs_launchSingleSciencePipeline.py', 'Science pipeline for input SCA image.');
 INSERT INTO pipelines (ppid,priority,script,descrip) values (12,4,'generateReferenceImage.py', 'Standard reference-image pipeline.');
+INSERT INTO pipelines (ppid,priority,script,descrip) values (17,5,'awsBatchSubmitJobs_launchSinglePostProcPipeline.py', 'Post-processing pipeline for input SCA image.');
