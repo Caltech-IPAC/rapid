7abb8c1e493f0222ef4a40c26dabc897bc2cb41e
Russ Laher <laher@ipac.caltech.edu>
2025-04-09 11:18:23 -0700

    First cut at a VPO for RAPID project.

---
+++ pipeline/virtualPipelineOperator.py
@@ -0,0 +1,499 @@
+"""
+Virtual Pipeline Operator (VPO) for the Rapid Pipeline Operations.
+
+To be executed inside a RAPID-pipeline Docker container.
+"""
+
+
+import sys
+import os
+import signal
+import configparser
+import boto3
+import re
+from datetime import datetime, timezone
+from dateutil import tz
+import time
+
+to_zone = tz.gettz('America/Los_Angeles')
+
+import modules.utils.rapid_pipeline_subs as util
+import database.modules.utils.rapid_db as db
+
+
+swname = "virtualPipelineOperator.py"
+swvers = "1.0"
+cfg_filename_only = "awsBatchSubmitJobs_launchSingleSciencePipeline.ini"
+
+
+# Specify python command to use for executing Python scripts.
+
+python_cmd = '/usr/bin/python3.11'
+launch_science_pipelines_code = '/code/pipeline/awsBatchSubmitJobs_launchSciencePipelinesForDateTimeRange.py'
+register_science_pipeline_jobs_code = '/code/pipeline/registerCompletedJobsInDB.py'
+launch_postproc_pipelines_code = 'awsBatchSubmitJobs_launchPostProcPipelinesForProcDate.py'
+register_postproc_pipeline_jobs_code = '/code/pipeline/registerCompletedJobsInDBAfterPostProc.py'
+
+
+# Print diagnostics.
+
+print("swname =", swname)
+print("swvers =", swvers)
+print("cfg_filename_only =", cfg_filename_only)
+print("python_cmd =", python_cmd)
+print("launch_science_pipelines_code =", launch_science_pipelines_code)
+print("register_science_pipeline_jobs_code =", register_science_pipeline_jobs_code)
+print("launch_postproc_pipelines_code =", launch_postproc_pipelines_code)
+print("register_postproc_pipeline_jobs_code =", register_postproc_pipeline_jobs_code)
+
+
+# Compute start time for benchmark.
+
+start_time_benchmark = time.time()
+
+
+# Compute processing datetime (UT) and processing datetime (Pacific time).
+
+datetime_utc_now = datetime.utcnow()
+proc_utc_datetime = datetime_utc_now.strftime('%Y-%m-%dT%H:%M:%SZ')
+datetime_pt_now = datetime_utc_now.replace(tzinfo=timezone.utc).astimezone(tz=to_zone)
+proc_pt_datetime_started = datetime_pt_now.strftime('%Y-%m-%dT%H:%M:%S PT')
+
+print("proc_utc_datetime =",proc_utc_datetime)
+print("proc_pt_datetime_started =",proc_pt_datetime_started)
+
+
+# Initialize handler.
+
+istop = 0
+
+def signal_handler(signum, frame):
+    print('Caught signal', signum)
+    global istop
+    istop = 1
+
+
+# Get processing date of interest from command-line argument.
+# This only needs to be given for running the VPO for just one specific processing date.
+
+try:
+    datearg = (sys.argv)[1]
+except IndexError:
+    datearg = None
+
+print("datearg =",datearg)
+
+
+# Read environment variables.
+
+rapid_sw = os.getenv('RAPID_SW')
+
+if rapid_sw is None:
+
+    print("*** Error: Env. var. RAPID_SW not set; quitting...")
+    exit(64)
+
+rapid_work = os.getenv('RAPID_WORK')
+
+if rapid_work is None:
+
+    print("*** Error: Env. var. RAPID_WORK not set; quitting...")
+    exit(64)
+
+cfg_path = rapid_sw + "/cdf"
+
+print("rapid_sw =",rapid_sw)
+print("cfg_path =",cfg_path)
+
+
+# Other required environment variables.
+
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+if aws_access_key_id is None:
+
+    print("*** Error: Env. var. AWS_ACCESS_KEY_ID not set; quitting...")
+    exit(64)
+
+if aws_secret_access_key is None:
+
+    print("*** Error: Env. var. AWS_SECRET_ACCESS_KEY not set; quitting...")
+    exit(64)
+
+
+# To process OpenUniverse simulation images, environment variables STARTDATETIME and ENDDATETIME
+# specify observation datetimes.  Later, this will be augmented with code to query the
+# SOCProcs database table for controlling the processing the Roman Space Telescope WFI data.
+#
+# Inputs are observaton start and end datetimes of exposures to be processed.
+# E.g., startdatetime = "2028-09-08 00:18:00", enddatetime = "2028-09-11 00:00:00"
+
+startdatetime = os.getenv('STARTDATETIME')
+
+if startdatetime is None:
+
+    print("*** Error: Env. var. STARTDATETIME not set; quitting...")
+    exit(64)
+
+enddatetime = os.getenv('ENDDATETIME')
+
+if enddatetime is None:
+
+    print("*** Error: Env. var. ENDDATETIME not set; quitting...")
+    exit(64)
+
+
+# Read input parameters from .ini file.
+
+config_input_filename = cfg_path + "/" + cfg_filename_only
+config_input = configparser.ConfigParser()
+config_input.read(config_input_filename)
+
+verbose = int(config_input['JOB_PARAMS']['verbose'])
+debug = int(config_input['JOB_PARAMS']['debug'])
+job_info_s3_bucket_base = config_input['JOB_PARAMS']['job_info_s3_bucket_base']
+job_logs_s3_bucket_base = config_input['JOB_PARAMS']['job_logs_s3_bucket_base']
+product_s3_bucket_base = config_input['JOB_PARAMS']['product_s3_bucket_base']
+job_config_filename_base = config_input['JOB_PARAMS']['job_config_filename_base']
+product_config_filename_base = config_input['JOB_PARAMS']['product_config_filename_base']
+awaicgen_output_mosaic_image_file = config_input['AWAICGEN']['awaicgen_output_mosaic_image_file']
+zogy_output_diffimage_file = config_input['ZOGY']['zogy_output_diffimage_file']
+
+
+# Set signal hander.
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGQUIT, signal_handler)
+
+
+
+
+
+
+
+
+def wait_until_aws_batch_jobs_finished(job_type,proc_date,config_input):
+
+    """"
+    Wait until AWS Batch jobs of a given job type and processing date have finished.
+    """
+
+
+    # Initialize iteration number.
+
+    iter = 0
+
+
+    # Set this parameter as fixed for the VPO.
+
+    job_status_to_list = "ALL"
+
+
+    # Define job definition.
+
+    if job_type == "science":
+        job_definition = config_input['AWS_BATCH']['job_definition']
+    elif job_type == "postproc":
+        job_definition = config_input['AWS_BATCH']['postproc_job_definition']
+    else:
+        print(f"*** Error: job_type not recognized (job_type={job_type}); quitting...")
+        exit(64)
+
+
+    # Define job queue.  Use AWS Batch Console to set this up once.
+
+    job_queue = config_input['AWS_BATCH']['job_queue']
+
+
+    # Get job name base.    Example job name: rapid_postproc_pipeline_20250404_jid997
+
+    if job_type == "science":
+        job_name_base = config_input['AWS_BATCH']['job_name_base']
+    elif job_type == "postproc":
+        job_name_base = config_input['AWS_BATCH']['postproc_job_name_base']
+    else:
+        print(f"*** Error: job_name_base not recognized (job_name_base={job_name_base}); quitting...")
+        exit(64)
+
+
+    # Print parameters.
+
+    print("Parameter values from method wait_until_aws_batch_jobs_finished:")
+    print("job_type =",job_type)
+    print("proc_date =",proc_date)
+    print("job_status_to_list =",job_status_to_list)
+    print("job_name_base =",job_name_base)
+    print("job_type =",job_type)
+    print("job_queue =",job_queue)
+    print("job_definition =",job_definition)
+    print("job_name_base =",job_name_base)
+
+
+    # Get Batch.Client object.
+
+    client = boto3.client('batch')
+
+    while True:
+
+        # Get list of jobs.
+        # The client.list_jobs method must be called the first time without the nextToken parameter.
+
+        page = 1
+        njobs_total = 0
+        if job_status_to_list != "ALL":
+            njobs_with_specified_status = 0
+        else:
+            njobs_vs_status = {}
+            for job_status in job_status_choices:
+                njobs_vs_status[job_status] = 0
+
+        job_name_wildcard = job_name_base + '*'
+
+
+        # Set next_token = None the first time util.list_aws_batch_jobs
+        # is called, so that proper behavior is handled by the method.
+
+        next_token = None
+        response = util.list_aws_batch_jobs(client,next_token,job_queue,job_name_wildcard)
+
+        for job in response['jobSummaryList']:
+            job_name = job['jobName']
+            if proc_date is None or proc_date in job_name:
+                job_status = job['status']
+                print("job_name,job_status =",job_name,job_status)
+                if job_status_to_list != "ALL" and job_status == job_status_to_list:
+                    njobs_with_specified_status += 1
+                else:
+                    njobs_vs_status[job_status] += 1
+
+                njobs_total += 1
+
+        next_token = response['nextToken']
+
+        print("page = ",page)
+
+        page += 1
+
+        while True:
+
+            response = util.list_aws_batch_jobs(client,next_token,job_queue,job_name_wildcard)
+
+            for job in response['jobSummaryList']:
+                job_name = job['jobName']
+                if proc_date is None or proc_date in job_name:
+                    job_status = job['status']
+                    print("job_name,job_status =",job_name,job_status)
+                    if job_status_to_list != "ALL" and job_status == job_status_to_list:
+                        njobs_with_specified_status += 1
+                    else:
+                        njobs_vs_status[job_status] += 1
+                    njobs_total += 1
+
+            # print("response = ",response)
+            # print("next_token = ",next_token)
+
+            print("page = ",page)
+
+            page += 1
+
+            try:
+                next_token = response['nextToken']
+            except:
+                break
+
+        print("njobs_total =",njobs_total)
+        print("job_status_to_list =",job_status_to_list)
+        if job_status_to_list != "ALL":
+            print(f"njobs_{job_status} = {njobs_with_specified_status}")
+        else:
+            for job_status in job_status_choices:
+                print(f"njobs_{job_status} = {njobs_vs_status[job_status]}")
+
+        njobs_succeeded_failed = njobs_vs_status["SUCCEEDED"] + njobs_vs_status["FAILED"]
+
+        if njobs_total == njobs_succeeded_failed:
+            break
+
+        iter += 1
+        print(f"From method wait_until_aws_batch_jobs_finished after iteration iter={iter}: Sleeping 30 seconds...")
+        time.sleep(30)
+
+    return
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------
+# Main program.
+#-------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+
+    # Open loop.
+
+    s3_client = boto3.client('s3')
+
+    exitcode = 0
+
+    i = 0
+
+    while True:
+
+        # Get current date and time.
+
+        datetime_utc_now = datetime.utcnow()
+        proc_utc_datetime = datetime_utc_now.strftime('%Y-%m-%dT%H:%M:%SZ')
+        datetime_pt_now = datetime_utc_now.replace(tzinfo=timezone.utc).astimezone(tz=to_zone)
+        proc_pt_datetime_started = datetime_pt_now.strftime('%Y-%m-%dT%H:%M:%S PT')
+        proc_pt_date = datetime_pt_now.strftime('%Y-%m-%d')
+
+        print("proc_utc_datetime =",proc_utc_datetime)
+        print("proc_pt_datetime_started =",proc_pt_datetime_started)
+        print("proc_pt_date =",proc_pt_date)
+
+        if datearg is None:
+            proc_date = proc_pt_date
+        else:
+            proc_date = datearg
+
+
+        """
+        # Open database connection.
+
+        dbh = db.RAPIDDB()
+
+        if dbh.exit_code >= 64:
+            exit(dbh.exit_code)
+
+
+
+
+        # Close database connection.
+
+        dbh.close()
+
+        if dbh.exit_code >= 64:
+            exit(dbh.exit_code)
+        """
+
+
+        # Launch science pipelines.
+        #
+        # Load environment variables STARTDATETIME and ENDDATETIME to specify observation datetimes.
+
+        os.environ['STARTDATETIME'] = startdatetime
+        os.environ['ENDDATETIME'] = enddatetime
+
+        launch_science_pipelines_cmd = [python_cmd,
+                                        launch_science_pipelines_code]
+
+        exitcode_from_launch_science_pipelines_cmd = util.execute_command(launch_science_pipelines_cmd)
+
+
+        # Wait for all science pipelines to complete under AWS Batch.
+
+        job_type = "science"
+
+        print(f"Waiting until AWS Batch jobs have finished for job_type={job_type}, proc_date={proc_date}...")
+
+        wait_until_aws_batch_jobs_finished(job_type,proc_date,config_input)
+
+        print(f"Okay, all AWS Batch jobs have finished for job_type={job_type}, proc_date={proc_date}...")
+
+
+        # Register metadata from science pipelines into operations database.
+
+        register_science_pipeline_jobs_cmd = [python_cmd,
+                                              register_science_pipeline_jobs_code,
+                                              proc_date]
+
+        exitcode_from_register_science_pipeline_jobs_cmd = util.execute_command(register_science_pipeline_jobs_cmd)
+
+
+        # Launch post-processing pipelines.
+        #
+        # Load environment variable JOBPROCDATE to specify processing date.
+
+        os.environ['JOBPROCDATE'] = proc_date
+
+        launch_postproc_pipelines_cmd = [python_cmd,
+                                        launch_postproc_pipelines_code]
+
+        exitcode_from_launch_postproc_pipelines_cmd = util.execute_command(launch_postproc_pipelines_cmd)
+
+
+        # Wait for all post-processing pipelines to complete under AWS Batch.
+
+        job_type = "postproc"
+
+        print(f"Waiting until AWS Batch jobs have finished for job_type={job_type}, proc_date={proc_date}...")
+
+        wait_until_aws_batch_jobs_finished(job_type,proc_date,config_input)
+
+        print(f"Okay, all AWS Batch jobs have finished for job_type={job_type}, proc_date={proc_date}...")
+
+
+        # Register metadata from post-processing pipelines into operations database.
+
+        register_postproc_pipeline_jobs_cmd = [python_cmd,
+                                              register_postproc_pipeline_jobs_code,
+                                              proc_date]
+
+        exitcode_from_register_postproc_pipeline_jobs_cmd = util.execute_command(register_postproc_pipeline_jobs_cmd)
+
+
+        # Break out of open loop if running the VPO for just one specific processing date.
+
+        if datearg is not None:
+            print(f"Terminating since this VPO run is just for one specific processing date: datearg={datearg}...")
+            break
+
+
+        # Test code.
+
+        a = 1
+        for n in range(10):
+            a = a + 1
+            print("n,a =",n,a)
+
+        print("Sleeping 30 seconds...")
+        time.sleep(30)
+        print("Waking up...")
+
+        if i == 3:
+            #os.kill(os.getpid(), signal.SIGQUIT)          # Quits gracefully via signal handler upon receiving control-/
+            #os.kill(os.getpid(), signal.SIGINT)           # Quits gracefully via signal handler upon receiving control-c
+            os.kill(os.getpid(), signal.SIGSTOP)           # Quits unconditionally and immediately.
+
+        if istop == 1:
+            print("Terminating gracefully now...")
+            exitcode = 7
+            exit(exitcode)
+
+        i += 1
+        print("i = ",i)
+
+
+        #
+        # End of open loop (but we are not iterating because of break above).
+        #
+
+
+    # Code-timing benchmark.
+
+    end_time_benchmark = time.time()
+    print("Elapsed time in seconds to launch pipelines =",
+        end_time_benchmark - start_time_benchmark)
+    start_time_benchmark = end_time_benchmark
+
+
+    # Termination.
+
+    print("Terminating: exitcode =",exitcode)
+
+    exit(exitcode)
+
