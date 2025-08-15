91823c9164e9ce99baf382da749d521f7604f34a
Russ Laher <laher@ipac.caltech.edu>
2025-08-15 07:49:10 -0700

    Added grants for Merges and AstroObjects tables.  Also add grant to allow pipeline software to create sources and astroobjects like-tables.

--- database/schema/rapidOpsSourcesTableGrants.sql
+++ database/schema/rapidOpsSourcesTableGrants.sql
@@ -8,6 +8,13 @@
 
 
 -------------------
+-- Allow pipeline software to create sources and astroobjects like-tables:
+-------------------
+
+GRANT CREATE ON SCHEMA public TO rapidporuss;
+
+
+-------------------
 -- Sources table
 -------------------
 
@@ -34,3 +41,52 @@ GRANT INSERT,UPDATE,SELECT,DELETE,TRUNCATE,TRIGGER,REFERENCES ON TABLE sources T
 REVOKE ALL ON SEQUENCE sources_sid_seq FROM rapidporole;
 GRANT USAGE ON SEQUENCE sources_sid_seq TO rapidporole;
 
+
+-------------------
+-- AstroObjects table
+-------------------
+
+-- rapidreadrole
+
+REVOKE ALL ON TABLE astroobjects FROM rapidreadrole;
+GRANT SELECT ON TABLE astroobjects TO GROUP rapidreadrole;
+
+REVOKE ALL ON SEQUENCE astroobjects_aid_seq FROM rapidreadrole;
+
+-- rapidadminrole
+
+REVOKE ALL ON TABLE astroobjects FROM rapidadminrole;
+GRANT ALL ON TABLE astroobjects TO GROUP rapidadminrole;
+
+REVOKE ALL ON SEQUENCE astroobjects_aid_seq FROM rapidadminrole;
+GRANT ALL ON SEQUENCE astroobjects_aid_seq TO GROUP rapidadminrole;
+
+-- rapidporole
+
+REVOKE ALL ON TABLE astroobjects FROM rapidporole;
+GRANT INSERT,UPDATE,SELECT,DELETE,TRUNCATE,TRIGGER,REFERENCES ON TABLE astroobjects TO rapidporole;
+
+REVOKE ALL ON SEQUENCE astroobjects_aid_seq FROM rapidporole;
+GRANT USAGE ON SEQUENCE astroobjects_aid_seq TO rapidporole;
+
+
+-------------------
+-- Merges table
+-------------------
+
+-- rapidreadrole
+
+REVOKE ALL ON TABLE merges FROM rapidreadrole;
+GRANT SELECT ON TABLE merges TO GROUP rapidreadrole;
+
+REVOKE ALL ON SEQUENCE merges_aid_seq FROM rapidreadrole;
+
+-- rapidadminrole
+
+REVOKE ALL ON TABLE merges FROM rapidadminrole;
+GRANT ALL ON TABLE merges TO GROUP rapidadminrole;
+
+-- rapidporole
+
+REVOKE ALL ON TABLE merges FROM rapidporole;
+GRANT INSERT,UPDATE,SELECT,DELETE,TRUNCATE,TRIGGER,REFERENCES ON TABLE merges TO rapidporole;
