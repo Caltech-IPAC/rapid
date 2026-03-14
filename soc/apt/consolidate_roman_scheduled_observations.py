fa3f04d0d605daaa504e2b860af66d2ec9b4e671
Russ Laher <laher@ipac.caltech.edu>
2026-03-14 08:20:19 -0700

    Added orient (E of North angle) column to conolidated CSV file.

--- soc/apt/consolidate_roman_scheduled_observations.py
+++ soc/apt/consolidate_roman_scheduled_observations.py
@@ -75,9 +75,12 @@ def get_visit_id_dict():
 
             jd_end = convert_to_jd(end)
 
+            pa = row["assigned orient"]
+
             visit_id_dict[row["visit-id"]] = []
             visit_id_dict[row["visit-id"]].append(jd_start)
             visit_id_dict[row["visit-id"]].append(jd_end)
+            visit_id_dict[row["visit-id"]].append(pa)
             visit_id_dict[row["visit-id"]].append(row["visit-id"])
 
     return visit_id_dict
@@ -101,6 +104,7 @@ def parse_pid_file(input_file,visit_id_dict):
     pids = []
     ra = []
     dec =[]
+    pas = []
     visit_id_list = []
     exposure_times = []
     ra_dec_dict = {}
@@ -130,7 +134,8 @@ def parse_pid_file(input_file,visit_id_dict):
 
             start_jd_obs.append(visit_jds[0])
             end_jd_obs.append(visit_jds[1])
-            visit_id_list.append(visit_jds[2])
+            pas.append(visit_jds[2])
+            visit_id_list.append(visit_jds[3])
 
             ra.append(row["RA"])
             dec.append(row["DEC"])
@@ -158,95 +163,104 @@ def parse_pid_file(input_file,visit_id_dict):
 
     print(f"pid,n_not_scheduled = {pid},{n_not_scheduled}")
 
-    return ra,dec,start_jd_obs,end_jd_obs,durations,bandpasses,pids,visit_id_list,exposure_times
+    return ra,dec,pas,start_jd_obs,end_jd_obs,durations,bandpasses,pids,visit_id_list,exposure_times
 
 
 
-def consolidate_surveys_in_schedule(ra1,dec1,visit_start_jd_obs1,visit_end_jd_obs1,durations1,bandpasses1,pids1,visits1,exposure_times1,
-                                    ra2,dec2,visit_start_jd_obs2,visit_end_jd_obs2,durations2,bandpasses2,pids2,visits2,exposure_times2,
-                                    ra3,dec3,visit_start_jd_obs3,visit_end_jd_obs3,durations3,bandpasses3,pids3,visits3,exposure_times3,
-                                    ra4,dec4,visit_start_jd_obs4,visit_end_jd_obs4,durations4,bandpasses4,pids4,visits4,exposure_times4):
+def consolidate_surveys_in_schedule(ra1,dec1,pas1,visit_start_jd_obs1,visit_end_jd_obs1,durations1,bandpasses1,pids1,visits1,exposure_times1,
+                                    ra2,dec2,pas2,visit_start_jd_obs2,visit_end_jd_obs2,durations2,bandpasses2,pids2,visits2,exposure_times2,
+                                    ra3,dec3,pas3,visit_start_jd_obs3,visit_end_jd_obs3,durations3,bandpasses3,pids3,visits3,exposure_times3,
+                                    ra4,dec4,pas4,visit_start_jd_obs4,visit_end_jd_obs4,durations4,bandpasses4,pids4,visits4,exposure_times4):
     visit_start_jds = []
     visit_end_jds = []
     durations = []
     ras = []
     decs = []
+    pas = []
     pids = []
     visits = []
     bandpasses = []
     durations = []
     exposure_times = []
 
-    for ra,dec,visit_start_jd_obs,visit_end_jd_obs,duration,bandpass,pid,visit,exposure_time in zip(ra1,
-                                                                                                    dec1,
-                                                                                                    visit_start_jd_obs1,
-                                                                                                    visit_end_jd_obs1,
-                                                                                                    durations1,
-                                                                                                    bandpasses1,
-                                                                                                    pids1,
-                                                                                                    visits1,
-                                                                                                    exposure_times1):
+    for ra,dec,pa,visit_start_jd_obs,visit_end_jd_obs,duration,bandpass,pid,visit,exposure_time in zip(ra1,
+                                                                                                       dec1,
+                                                                                                       pas1,
+                                                                                                       visit_start_jd_obs1,
+                                                                                                       visit_end_jd_obs1,
+                                                                                                       durations1,
+                                                                                                       bandpasses1,
+                                                                                                       pids1,
+                                                                                                       visits1,
+                                                                                                       exposure_times1):
         visit_start_jds.append(visit_start_jd_obs)
         visit_end_jds.append(visit_end_jd_obs)
         ras.append(ra)
         decs.append(dec)
+        pas.append(pa)
         pids.append(pid)
         visits.append(visit)
         durations.append(duration)
         bandpasses.append(bandpass)
         exposure_times.append(exposure_time)
 
-    for ra,dec,visit_start_jd_obs,visit_end_jd_obs,duration,bandpass,pid,visit,exposure_time in zip(ra2,
-                                                                                                    dec2,
-                                                                                                    visit_start_jd_obs2,
-                                                                                                    visit_end_jd_obs2,
-                                                                                                    durations2,
-                                                                                                    bandpasses2,
-                                                                                                    pids2,
-                                                                                                    visits2,
-                                                                                                    exposure_times2):
+    for ra,dec,pa,visit_start_jd_obs,visit_end_jd_obs,duration,bandpass,pid,visit,exposure_time in zip(ra2,
+                                                                                                       dec2,
+                                                                                                       pas2,
+                                                                                                       visit_start_jd_obs2,
+                                                                                                       visit_end_jd_obs2,
+                                                                                                       durations2,
+                                                                                                       bandpasses2,
+                                                                                                       pids2,
+                                                                                                       visits2,
+                                                                                                       exposure_times2):
         visit_start_jds.append(visit_start_jd_obs)
         visit_end_jds.append(visit_end_jd_obs)
         ras.append(ra)
         decs.append(dec)
+        pas.append(pa)
         pids.append(pid)
         visits.append(visit)
         durations.append(duration)
         bandpasses.append(bandpass)
         exposure_times.append(exposure_time)
 
-    for ra,dec,visit_start_jd_obs,visit_end_jd_obs,duration,bandpass,pid,visit,exposure_time in zip(ra3,
-                                                                                                    dec3,
-                                                                                                    visit_start_jd_obs3,
-                                                                                                    visit_end_jd_obs3,
-                                                                                                    durations3,
-                                                                                                    bandpasses3,
-                                                                                                    pids3,
-                                                                                                    visits3,
-                                                                                                    exposure_times3):
+    for ra,dec,pa,visit_start_jd_obs,visit_end_jd_obs,duration,bandpass,pid,visit,exposure_time in zip(ra3,
+                                                                                                       dec3,
+                                                                                                       pas3,
+                                                                                                       visit_start_jd_obs3,
+                                                                                                       visit_end_jd_obs3,
+                                                                                                       durations3,
+                                                                                                       bandpasses3,
+                                                                                                       pids3,
+                                                                                                       visits3,
+                                                                                                       exposure_times3):
         visit_start_jds.append(visit_start_jd_obs)
         visit_end_jds.append(visit_end_jd_obs)
         ras.append(ra)
         decs.append(dec)
+        pas.append(pa)
         pids.append(pid)
         visits.append(visit)
         durations.append(duration)
         bandpasses.append(bandpass)
         exposure_times.append(exposure_time)
 
-    for ra,dec,visit_start_jd_obs,visit_end_jd_obs,duration,bandpass,pid,visit,exposure_time in zip(ra4,
-                                                                                                    dec4,
-                                                                                                    visit_start_jd_obs4,
-                                                                                                    visit_end_jd_obs4,
-                                                                                                    durations4,
-                                                                                                    bandpasses4,
-                                                                                                    pids4,
-                                                                                                    visits4,
-                                                                                                    exposure_times4):
+    for ra,dec,pa,visit_start_jd_obs,visit_end_jd_obs,duration,bandpass,pid,visit,exposure_time in zip(ra4,
+                                                                                                       dec4,
+                                                                                                       pas4,
+                                                                                                       visit_start_jd_obs4,
+                                                                                                       visit_end_jd_obs4,
+                                                                                                       durations4,
+                                                                                                       bandpasses4,
+                                                                                                       pids4,
+                                                                                                       visits4,
+                                                                                                       exposure_times4):
         visit_start_jds.append(visit_start_jd_obs)
         visit_end_jds.append(visit_end_jd_obs)
         ras.append(ra)
         decs.append(dec)
+        pas.append(pa)
         pids.append(pid)
         visits.append(visit)
         durations.append(duration)
@@ -266,7 +280,7 @@ def consolidate_surveys_in_schedule(ra1,dec1,visit_start_jd_obs1,visit_end_jd_ob
 
     # Headers for CVS output file.
 
-    sr = ["index","visit","pid","survey","ra","dec","visit_start_jd","visit_end_jd",
+    sr = ["index","visit_id","pid","survey","ra","dec","orient","visit_start_jd","visit_end_jd",
           "exposure_duration[day]","bandpass","exposure_start_obs_jd","exposure_end_obs_jd",
           "exposure_time[s]","delta_jd"]
     schedule_rows.append(sr)
@@ -300,6 +314,7 @@ def consolidate_surveys_in_schedule(ra1,dec1,visit_start_jd_obs1,visit_end_jd_ob
 
         ra = ras[sort_index]
         dec = decs[sort_index]
+        pa = pas[sort_index]
         pid = int(pids[sort_index])
         survey = surveys_dict[pid]
         visit = visits[sort_index]
@@ -335,7 +350,7 @@ def consolidate_surveys_in_schedule(ra1,dec1,visit_start_jd_obs1,visit_end_jd_ob
         else:
 
             j += 1
-            sr = [j,visit,pid,survey,ra,dec,visit_start_jd,visit_end_jd,
+            sr = [j,visit,pid,survey,ra,dec,pa,visit_start_jd,visit_end_jd,
                   duration,bandpass,exposure_start_obs_jd,exposure_end_obs_jd,
                   exposure_time,delta_jd]
             print(sr)
@@ -407,13 +422,13 @@ if __name__ == '__main__':
 
     # Parse PID<nnn>.sim.ecsv
 
-    ra1,dec1,start_jd_obs1,end_jd_obs1,durations1,bandpasses1,pids1,visits1,exposure_times1 = \
+    ra1,dec1,pas1,start_jd_obs1,end_jd_obs1,durations1,bandpasses1,pids1,visits1,exposure_times1 = \
         parse_pid_file(input_files[0],visit_id_dict)
-    ra2,dec2,start_jd_obs2,end_jd_obs2,durations2,bandpasses2,pids2,visits2,exposure_times2 = \
+    ra2,dec2,pas2,start_jd_obs2,end_jd_obs2,durations2,bandpasses2,pids2,visits2,exposure_times2 = \
         parse_pid_file(input_files[1],visit_id_dict)
-    ra3,dec3,start_jd_obs3,end_jd_obs3,durations3,bandpasses3,pids3,visits3,exposure_times3 = \
+    ra3,dec3,pas3,start_jd_obs3,end_jd_obs3,durations3,bandpasses3,pids3,visits3,exposure_times3 = \
         parse_pid_file(input_files[2],visit_id_dict)
-    ra4,dec4,start_jd_obs4,end_jd_obs4,durations4,bandpasses4,pids4,visits4,exposure_times4 = \
+    ra4,dec4,pas4,start_jd_obs4,end_jd_obs4,durations4,bandpasses4,pids4,visits4,exposure_times4 = \
         parse_pid_file(input_files[3],visit_id_dict)
 
 
@@ -421,9 +436,9 @@ if __name__ == '__main__':
     Consolidate surveys in schedule.
     '''
 
-    consolidate_surveys_in_schedule(ra1,dec1,start_jd_obs1,end_jd_obs1,durations1,bandpasses1,pids1,visits1,exposure_times1,
-                                    ra2,dec2,start_jd_obs2,end_jd_obs2,durations2,bandpasses2,pids2,visits2,exposure_times2,
-                                    ra3,dec3,start_jd_obs3,end_jd_obs3,durations3,bandpasses3,pids3,visits3,exposure_times3,
-                                    ra4,dec4,start_jd_obs4,end_jd_obs4,durations4,bandpasses4,pids4,visits4,exposure_times4)
+    consolidate_surveys_in_schedule(ra1,dec1,pas1,start_jd_obs1,end_jd_obs1,durations1,bandpasses1,pids1,visits1,exposure_times1,
+                                    ra2,dec2,pas2,start_jd_obs2,end_jd_obs2,durations2,bandpasses2,pids2,visits2,exposure_times2,
+                                    ra3,dec3,pas3,start_jd_obs3,end_jd_obs3,durations3,bandpasses3,pids3,visits3,exposure_times3,
+                                    ra4,dec4,pas4,start_jd_obs4,end_jd_obs4,durations4,bandpasses4,pids4,visits4,exposure_times4)
 
 
