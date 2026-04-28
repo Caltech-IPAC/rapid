f8bb4903d388fe53209d9ae985847a97f1c63a86
Russ Laher <laher@ipac.caltech.edu>
2026-04-28 11:35:28 -0700

    Renamed method add_l2file to add_l2file_fourth_order.  Added new method add_l2file_fifth_order.

--- database/modules/utils/rapid_db.py
+++ database/modules/utils/rapid_db.py
@@ -289,9 +289,10 @@ class RAPIDDB:
             self.conn.commit()           # Commit database transaction
 
 
+
 ########################################################################################################
 
-    def add_l2file(self,expid,sca,field,hp6,hp9,fid,dateobs,mjdobs,exptime,infobits,
+    def add_l2file_fourth_order(self,expid,sca,field,hp6,hp9,fid,dateobs,mjdobs,exptime,infobits,
         status,filename,checksum,crval1,crval2,crpix1,crpix2,cd11,cd12,cd21,cd22,
         ctype1,ctype2,cunit1,cunit2,a_order,a_0_2,a_0_3,a_0_4,a_1_1,a_1_2,
         a_1_3,a_2_0,a_2_1,a_2_2,a_3_0,a_3_1,a_4_0,b_order,b_0_2,b_0_3,
@@ -429,6 +430,211 @@ class RAPIDDB:
         rep["TEMPLATE_B_3_0"] = str(b_3_0)
         rep["TEMPLATE_B_3_1"] = str(b_3_1)
         rep["TEMPLATE_B_4_0"] = str(b_4_0)
+        rep["TEMPLATE_EQUINOX"] = str(equinox)
+        rep["TEMPLATE_RA"] = str(ra)
+        rep["TEMPLATE_DEC"] = str(dec)
+        rep["TEMPLATE_PAOBSY"] = str(paobsy)
+        rep["TEMPLATE_PAFPA"] = str(pafpa)
+        rep["TEMPLATE_ZPTMAG"] = str(zptmag)
+        rep["TEMPLATE_SKYMEAN"] = str(skymean)
+
+        rep = dict((re.escape(k), v) for k, v in rep.items())
+        pattern = re.compile("|".join(rep.keys()))
+        query = pattern.sub(lambda m: rep[re.escape(m.group(0))], query_template)
+
+        print('query = {}'.format(query))
+
+        self.cur.execute(query)
+        record = self.cur.fetchone()
+
+        if record is not None:
+            self.rid = record[0]
+            self.version = record[1]
+        else:
+            self.rid = None
+            self.version = None
+            print("*** Error: Could not insert L2Files record; returning...")
+            self.exit_code = 67
+            return
+
+        if self.exit_code == 0:
+            self.conn.commit()           # Commit database transaction
+
+
+########################################################################################################
+
+    def add_l2file_fifth_order(self,expid,sca,field,hp6,hp9,fid,dateobs,mjdobs,exptime,infobits,
+        status,filename,checksum,crval1,crval2,crpix1,crpix2,cd11,cd12,cd21,cd22,
+        ctype1,ctype2,cunit1,cunit2,
+        a_order,a_0_1,a_0_2,a_0_3,a_0_4,a_0_5,a_1_0,a_1_1,a_1_2,a_1_3,a_1_4,
+        a_2_0,a_2_1,a_2_2,a_2_3,a_3_0,a_3_1,a_3_2,a_4_0,a_4_1,a_5_0,
+        b_order,b_0_1,b_0_2,b_0_3,b_0_4,b_0_5,b_1_0,b_1_1,b_1_2,b_1_3,b_1_4,
+        b_2_0,b_2_1,b_2_2,b_2_3,b_3_0,b_3_1,b_3_2,b_4_0,b_4_1,b_5_0,
+        equinox,ra,dec,paobsy,pafpa,zptmag,skymean):
+
+        '''
+        Add record in L2files database table.
+        '''
+
+        self.exit_code = 0
+
+
+        # Define query template.
+
+        query_template =\
+            "select * from addL2File(" +\
+            "cast(TEMPLATE_EXPID as integer)," +\
+            "cast(TEMPLATE_SCA as smallint)," +\
+            "cast(TEMPLATE_FIELD as integer)," +\
+            "cast(TEMPLATE_HP6 as integer)," +\
+            "cast(TEMPLATE_HP9 as integer)," +\
+            "cast(TEMPLATE_FID as smallint)," +\
+            "cast('TEMPLATE_DATEOBS' as timestamp without time zone)," +\
+            "cast(TEMPLATE_MJDOBS as double precision)," +\
+            "cast(TEMPLATE_EXPTIME as real)," +\
+            "cast(TEMPLATE_INFOBITS as integer)," +\
+            "cast('TEMPLATE_FILENAME' as character varying(255))," +\
+            "cast('TEMPLATE_CHECKSUM' as character varying(32))," +\
+            "cast(TEMPLATE_STATUS as smallint)," +\
+            "cast(TEMPLATE_CRVAL1 as double precision)," +\
+            "cast(TEMPLATE_CRVAL2 as double precision)," +\
+            "cast(TEMPLATE_CRPIX1 as real)," +\
+            "cast(TEMPLATE_CRPIX2 as real)," +\
+            "cast(TEMPLATE_CD11 as double precision)," +\
+            "cast(TEMPLATE_CD12 as double precision)," +\
+            "cast(TEMPLATE_CD21 as double precision)," +\
+            "cast(TEMPLATE_CD22 as double precision)," +\
+            "cast('TEMPLATE_CTYPE1' as character varying(16))," +\
+            "cast('TEMPLATE_CTYPE2' as character varying(16))," +\
+            "cast('TEMPLATE_CUNIT1' as character varying(16))," +\
+            "cast('TEMPLATE_CUNIT2' as character varying(16))," +\
+            "cast(TEMPLATE_A_ORDER as smallint)," +\
+            "cast(TEMPLATE_A_0_1 as double precision)," +\
+            "cast(TEMPLATE_A_0_2 as double precision)," +\
+            "cast(TEMPLATE_A_0_3 as double precision)," +\
+            "cast(TEMPLATE_A_0_4 as double precision)," +\
+            "cast(TEMPLATE_A_0_5 as double precision)," +\
+            "cast(TEMPLATE_A_1_0 as double precision)," +\
+            "cast(TEMPLATE_A_1_1 as double precision)," +\
+            "cast(TEMPLATE_A_1_2 as double precision)," +\
+            "cast(TEMPLATE_A_1_3 as double precision)," +\
+            "cast(TEMPLATE_A_1_4 as double precision)," +\
+            "cast(TEMPLATE_A_2_0 as double precision)," +\
+            "cast(TEMPLATE_A_2_1 as double precision)," +\
+            "cast(TEMPLATE_A_2_2 as double precision)," +\
+            "cast(TEMPLATE_A_2_3 as double precision)," +\
+            "cast(TEMPLATE_A_3_0 as double precision)," +\
+            "cast(TEMPLATE_A_3_1 as double precision)," +\
+            "cast(TEMPLATE_A_3_2 as double precision)," +\
+            "cast(TEMPLATE_A_4_0 as double precision)," +\
+            "cast(TEMPLATE_A_4_1 as double precision)," +\
+            "cast(TEMPLATE_A_5_0 as double precision)," +\
+            "cast(TEMPLATE_B_ORDER as smallint)," +\
+            "cast(TEMPLATE_B_0_1 as double precision)," +\
+            "cast(TEMPLATE_B_0_2 as double precision)," +\
+            "cast(TEMPLATE_B_0_3 as double precision)," +\
+            "cast(TEMPLATE_B_0_4 as double precision)," +\
+            "cast(TEMPLATE_B_0_5 as double precision)," +\
+            "cast(TEMPLATE_B_1_0 as double precision)," +\
+            "cast(TEMPLATE_B_1_1 as double precision)," +\
+            "cast(TEMPLATE_B_1_2 as double precision)," +\
+            "cast(TEMPLATE_B_1_3 as double precision)," +\
+            "cast(TEMPLATE_B_1_4 as double precision)," +\
+            "cast(TEMPLATE_B_2_0 as double precision)," +\
+            "cast(TEMPLATE_B_2_1 as double precision)," +\
+            "cast(TEMPLATE_B_2_2 as double precision)," +\
+            "cast(TEMPLATE_B_2_3 as double precision)," +\
+            "cast(TEMPLATE_B_3_0 as double precision)," +\
+            "cast(TEMPLATE_B_3_1 as double precision)," +\
+            "cast(TEMPLATE_B_3_2 as double precision)," +\
+            "cast(TEMPLATE_B_4_0 as double precision)," +\
+            "cast(TEMPLATE_B_4_1 as double precision)," +\
+            "cast(TEMPLATE_B_5_0 as double precision)," +\
+            "cast(TEMPLATE_EQUINOX as real)," +\
+            "cast(TEMPLATE_RA as double precision)," +\
+            "cast(TEMPLATE_DEC as double precision)," +\
+            "cast(TEMPLATE_PAOBSY as real)," +\
+            "cast(TEMPLATE_PAFPA as real)," +\
+            "cast(TEMPLATE_ZPTMAG as real)," +\
+            "cast(TEMPLATE_SKYMEAN AS real)) as " +\
+            "(rid integer," +\
+            " version smallint);"
+
+
+        # Query database.
+
+        print('----> expid = {}'.format(expid))
+        print('----> sca = {}'.format(sca))
+        print('----> filename = {}'.format(filename))
+
+        rep = {"TEMPLATE_EXPID": str(expid),
+               "TEMPLATE_SCA": str(sca),
+               "TEMPLATE_FIELD": str(field),
+               "TEMPLATE_HP6": str(hp6),
+               "TEMPLATE_HP9": str(hp9),
+               "TEMPLATE_FID": str(fid),
+               "TEMPLATE_DATEOBS": dateobs}
+
+        rep["TEMPLATE_MJDOBS"] = str(mjdobs)
+        rep["TEMPLATE_EXPTIME"] = str(exptime)
+        rep["TEMPLATE_INFOBITS"] = str(infobits)
+        rep["TEMPLATE_FILENAME"] = filename
+        rep["TEMPLATE_CHECKSUM"] = checksum
+        rep["TEMPLATE_STATUS"] = str(status)
+        rep["TEMPLATE_CRVAL1"] = str(crval1)
+        rep["TEMPLATE_CRVAL2"] = str(crval2)
+        rep["TEMPLATE_CRPIX1"] = str(crpix1)
+        rep["TEMPLATE_CRPIX2"] = str(crpix2)
+        rep["TEMPLATE_CD11"] = str(cd11)
+        rep["TEMPLATE_CD12"] = str(cd12)
+        rep["TEMPLATE_CD21"] = str(cd21)
+        rep["TEMPLATE_CD22"] = str(cd22)
+        rep["TEMPLATE_CTYPE1"] = ctype1
+        rep["TEMPLATE_CTYPE2"] = ctype2
+        rep["TEMPLATE_CUNIT1"] = cunit1
+        rep["TEMPLATE_CUNIT2"] = cunit2
+        rep["TEMPLATE_A_ORDER"] = str(a_order)
+        rep["TEMPLATE_A_0_1"] = str(a_0_1)
+        rep["TEMPLATE_A_0_2"] = str(a_0_2)
+        rep["TEMPLATE_A_0_3"] = str(a_0_3)
+        rep["TEMPLATE_A_0_4"] = str(a_0_4)
+        rep["TEMPLATE_A_0_5"] = str(a_0_5)
+        rep["TEMPLATE_A_1_0"] = str(a_1_0)
+        rep["TEMPLATE_A_1_1"] = str(a_1_1)
+        rep["TEMPLATE_A_1_2"] = str(a_1_2)
+        rep["TEMPLATE_A_1_3"] = str(a_1_3)
+        rep["TEMPLATE_A_1_4"] = str(a_1_4)
+        rep["TEMPLATE_A_2_0"] = str(a_2_0)
+        rep["TEMPLATE_A_2_1"] = str(a_2_1)
+        rep["TEMPLATE_A_2_2"] = str(a_2_2)
+        rep["TEMPLATE_A_2_3"] = str(a_2_3)
+        rep["TEMPLATE_A_3_0"] = str(a_3_0)
+        rep["TEMPLATE_A_3_1"] = str(a_3_1)
+        rep["TEMPLATE_A_3_2"] = str(a_3_2)
+        rep["TEMPLATE_A_4_0"] = str(a_4_0)
+        rep["TEMPLATE_A_4_1"] = str(a_4_1)
+        rep["TEMPLATE_A_5_0"] = str(a_5_0)
+        rep["TEMPLATE_B_ORDER"] = str(b_order)
+        rep["TEMPLATE_B_0_1"] = str(b_0_1)
+        rep["TEMPLATE_B_0_2"] = str(b_0_2)
+        rep["TEMPLATE_B_0_3"] = str(b_0_3)
+        rep["TEMPLATE_B_0_4"] = str(b_0_4)
+        rep["TEMPLATE_B_0_5"] = str(b_0_5)
+        rep["TEMPLATE_B_1_0"] = str(b_1_0)
+        rep["TEMPLATE_B_1_1"] = str(b_1_1)
+        rep["TEMPLATE_B_1_2"] = str(b_1_2)
+        rep["TEMPLATE_B_1_3"] = str(b_1_3)
+        rep["TEMPLATE_B_1_4"] = str(b_1_4)
+        rep["TEMPLATE_B_2_0"] = str(b_2_0)
+        rep["TEMPLATE_B_2_1"] = str(b_2_1)
+        rep["TEMPLATE_B_2_2"] = str(b_2_2)
+        rep["TEMPLATE_B_2_3"] = str(b_2_3)
+        rep["TEMPLATE_B_3_0"] = str(b_3_0)
+        rep["TEMPLATE_B_3_1"] = str(b_3_1)
+        rep["TEMPLATE_B_3_2"] = str(b_3_2)
+        rep["TEMPLATE_B_4_0"] = str(b_4_0)
+        rep["TEMPLATE_B_4_1"] = str(b_4_1)
+        rep["TEMPLATE_B_5_0"] = str(b_5_0)
         rep["TEMPLATE_EQUINOX"] = str(equinox)
         rep["TEMPLATE_RA"] = str(ra)
         rep["TEMPLATE_DEC"] = str(dec)
