2f899171bc0f3967dd5f7684a32c86cacea6cfa8
Russ Laher <laher@ipac.caltech.edu>
2026-07-20 09:07:52 -0700

    Fixed typo in AstroObjectsMeta in print message.

--- pipeline/computeStatisticsForAstroObjects.py
+++ pipeline/computeStatisticsForAstroObjects.py
@@ -372,7 +372,7 @@ def run_single_core_job(fields,index_thread):
                 stddec = np.std(filtered_decs_list)
                 stdflux = np.std(filtered_fluxes_list)
 
-                print(f"Inserting AstrObjectsMeta record: astroobjectsmeta_tablename,aid," +
+                print(f"Inserting AstroObjectsMeta record: astroobjectsmeta_tablename,aid," +
                       f"meanra,meandec,nsources={astroobjectsmeta_tablename},{aid},{meanra},{meandec},{nsources}")
 
                 dbh.insert_astroobjectsmeta_statistics(astroobjectsmeta_tablename,
