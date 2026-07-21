40c832445fe910c8bdfaca1895b27c5164d318cb
Russ Laher <laher@ipac.caltech.edu>
2026-07-21 11:04:56 -0700

    Modified to open DB connection in thread method. Improved handling of num_cores.  Replaced exits with raise exceptions in thread method.

--- pipeline/loadPSFCatIntoDBSourcesTable.py
+++ pipeline/loadPSFCatIntoDBSourcesTable.py
@@ -144,7 +144,7 @@ naxis2 = int(config_input['INSTRUMENT']['naxis2_sciimage'])
 ppid = int(config_input['SCI_IMAGE']['ppid'])
 
 
-# Open database connections for parallel access.
+# Get number of cores for parallel processing.
 
 num_cores = os.getenv('NUM_CORES')
 
@@ -154,17 +154,6 @@ else:
     num_cores = int(num_cores)
 
 print("num_cores =",num_cores)
-
-dbh_list = []
-
-for i in range(num_cores):
-
-    dbh = db.RAPIDDB()
-
-    if dbh.exit_code >= 64:
-        exit(dbh.exit_code)
-
-    dbh_list.append(dbh)
 
 
 # Get S3 client.
@@ -242,9 +231,18 @@ def run_single_core_job(jids,overlapping_fields_list,meta_list,negative_diffimg_
         fh = open(thread_work_file, 'w', encoding="utf-8")
     except:
         print(f"*** Error: Could not open output file {thread_work_file}; quitting...")
-        exit(64)
+        raise RuntimeError(f"*** Error: Could not open output file {thread_work_file}; quitting...")
 
-    dbh = dbh_list[index_thread]
+
+    # Open database connection.
+
+    dbh = db.RAPIDDB()
+
+    if dbh.exit_code >= 64:
+        fh.write(f"*** Error opening database connection (dbh.exit_code={dbh.exit_code}); quitting...\n")
+        fh.flush()
+        raise RuntimeError(f"*** Error opening database connection (dbh.exit_code={dbh.exit_code}); quitting...\n")
+
 
     fh.write(f"\nStart of run_single_core_job: index_thread={index_thread}, dbh={dbh}\n")
 
@@ -478,12 +476,24 @@ def run_single_core_job(jids,overlapping_fields_list,meta_list,negative_diffimg_
 
             if os.path.exists(file_path):
                 os.remove(file_path)
-                print(f"File deleted successfully ({file_path}).")
+                fh.write(f"File deleted successfully ({file_path})...\n")
+                fh.flush()
             else:
-                print(f"The file does not exist({file_path}).")
+                fh.write(f"The file does not exist({file_path})...\n")
+                fh.flush()
 
 
         # End of loop over job ID.
+
+
+    # Close database connection.
+
+    dbh.close()
+
+    if dbh.exit_code >= 64:
+        fh.write(f"*** Error closing database connection (dbh.exit_code={dbh.exit_code}); quitting...\n")
+        fh.flush()
+        raise RuntimeError(f"*** Error closing database connection (dbh.exit_code={dbh.exit_code}); quitting...")
 
 
     fh.write(f"\nEnd of run_single_core_job: index_thread={index_thread}\n")
@@ -495,10 +505,7 @@ def run_single_core_job(jids,overlapping_fields_list,meta_list,negative_diffimg_
     return message
 
 
-def execute_parallel_processes(jids,rtids_list,meta_list,negative_diffimg_flag,num_cores=None):
-
-    if num_cores is None:
-        num_cores = os.cpu_count()  # Use all available cores if not specified
+def execute_parallel_processes(jids,rtids_list,meta_list,negative_diffimg_flag,num_cores):
 
     print("num_cores =",num_cores)
 
@@ -783,18 +790,12 @@ if __name__ == '__main__':
         end_time_benchmark - start_time_benchmark_at_start)
 
 
-    # Close database connections.
+    # Close database connection.
 
     dbh.close()
 
     if dbh.exit_code >= 64:
         exit(dbh.exit_code)
-
-    for tdbh in dbh_list:
-        tdbh.close()
-
-        if tdbh.exit_code >= 64:
-            exit(tdbh.exit_code)
 
 
     # Termination.
