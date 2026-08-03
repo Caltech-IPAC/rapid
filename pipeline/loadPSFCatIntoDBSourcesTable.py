396faa221e66ed06fd07fd4fe33dcfe57d5c7f1e
Azalee Bostroem <bostroem@ipac.caltech.edu>
2026-08-03 10:19:10 -0700

    resolving mixed variable naming in table_to_buffer function

--- pipeline/loadPSFCatIntoDBSourcesTable.py
+++ pipeline/loadPSFCatIntoDBSourcesTable.py
@@ -199,8 +199,8 @@ def table_to_buffer(tbl, colnames, null_string=r"\N", separator=","):
         #use %r to create a string with minimum number of digits
         column_data_str = np.char.mod('%r', column_data) if column_data.dtype.kind == 'f' else column_data.astype(str)
         if column_data.dtype.kind == 'f':
-            s = np.where(np.isnan(column_data), null_string, column_data_str)      # NaN -> NULL at the source
-        cols.append(s)
+            column_data_str = np.where(np.isnan(column_data), null_string, column_data_str)      # NaN -> NULL at the source
+        cols.append(column_data_str)
     buf = io.StringIO()
     buf.write("\n".join(separator.join(row) for row in zip(*cols)))
     buf.write("\n")
