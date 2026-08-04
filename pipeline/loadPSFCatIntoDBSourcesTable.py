f65adcfa6fa6df368a340602ea0cf8934f215865
Azalee Bostroem <bostroem@ipac.caltech.edu>
2026-08-03 17:30:51 -0700

    removing replacing NaN with NULL

--- pipeline/loadPSFCatIntoDBSourcesTable.py
+++ pipeline/loadPSFCatIntoDBSourcesTable.py
@@ -198,8 +198,6 @@ def table_to_buffer(tbl, colnames, null_string=r"\N", separator=","):
         column_data = np.asarray(tbl[name])
         #use %r to create a string with minimum number of digits
         column_data_str = np.char.mod('%s', column_data) if column_data.dtype.kind == 'f' else column_data.astype(str)
-        if column_data.dtype.kind == 'f':
-            column_data_str = np.where(np.isnan(column_data), null_string, column_data_str)      # NaN -> NULL at the source
         cols.append(column_data_str)
     buf = io.StringIO()
     buf.write("\n".join(separator.join(row) for row in zip(*cols)))
