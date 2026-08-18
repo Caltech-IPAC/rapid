703fd1a2debcbc237233cb1cbd12d74c67bea9ab
Russ Laher <laher@ipac.caltech.edu>
2026-08-18 15:15:58 -0700

    Updated call to method util.lookup_source_tables_to_crossmatch_and_distinct_fields

--- pipeline/pruneNotBestMerges.py
+++ pipeline/pruneNotBestMerges.py
@@ -347,8 +347,8 @@ if __name__ == '__main__':
     # Look up for the given processing date the Sources child table names
     # that were cross-matched and a distinct list of the fields covered by the sources.
 
-    source_tables_to_crossmatch_tuples_list,fields_list,_ = \
-        util.lookup_source_tables_to_crossmatch_and_distinct_fields(dbh,proc_date)
+    source_tables_to_crossmatch_tuples_list,fields_list,_,_ = \
+        util.lookup_source_tables_to_crossmatch_and_distinct_fields(dbh,proc_date,ppid)
 
     sources_child_tables = []
     for table_to_crossmatch_tuple in source_tables_to_crossmatch_tuples_list:
