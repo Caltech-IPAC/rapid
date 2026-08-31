cfc5885b6506899241b1331fc6bdf85239dc42ca
Russ Laher <laher@ipac.caltech.edu>
2026-08-31 12:25:20 -0700

    Finalized the list of parameters.

--- scripts/parse_sexcat.py
+++ scripts/parse_sexcat.py
@@ -4,8 +4,10 @@ import modules.utils.rapid_pipeline_subs as util
 datafile="/Users/laher/Folks/rapid/sfftdiffimage_masked.txt";
 paramsfile="/Users/laher/git/rapid/cdf/rapidSexParamsDiffImage.inp";
 
-params_to_get = ["X_WORLD",
-                 "Y_WORLD",
+params_to_get = ["NUMBER",
+                 "FLAGS",
+                 "ALPHAWIN_J2000",
+                 "DELTAWIN_J2000",
                  "XWIN_IMAGE",
                  "YWIN_IMAGE",
                  "AWIN_WORLD",
@@ -24,7 +26,7 @@ params_to_get = ["X_WORLD",
                  "FLUXERR_APER_3",
                  "FLUXERR_APER_4",
                  "FLUXERR_APER_5",
-                ]
+                 ]
 
 vals = util.parse_ascii_text_sextractor_catalog(datafile,paramsfile,params_to_get)
 
@@ -41,38 +43,37 @@ with open('test_parse2.txt', 'w') as csvfile:
 
     for i in range(num_rows):
 
-        x_world = vals[i][0]
-        y_world = vals[i][1]
-        xwin_image = vals[i][2]
-        ywin_image = vals[i][3]
-        awin_world = vals[i][4]
-        bwin_world = vals[i][5]
-        awin_image = vals[i][6]
-        bwin_image = vals[i][7]
-        fwhm_image = vals[i][8]
-        class_star = vals[i][9]
-        flux_aper_1 = vals[i][10]
-        flux_aper_2 = vals[i][11]
-        flux_aper_3 = vals[i][12]
-        flux_aper_4 = vals[i][13]
-        flux_aper_5 = vals[i][14]
-        fluxerr_aper_1 = vals[i][15]
-        fluxerr_aper_2 = vals[i][16]
-        fluxerr_aper_3 = vals[i][17]
-        fluxerr_aper_4 = vals[i][18]
-        fluxerr_aper_5 = vals[i][19]
+        number = vals[i][0]
+        flags = vals[i][1]
+        alphawin_j2000 = vals[i][2]
+        deltawin_j2000 = vals[i][3]
+        xwin_image = vals[i][4]
+        ywin_image = vals[i][5]
+        awin_world = vals[i][6]
+        bwin_world = vals[i][7]
+        awin_image = vals[i][8]
+        bwin_image = vals[i][9]
+        fwhm_image = vals[i][10]
+        class_star = vals[i][11]
+        flux_aper_1 = vals[i][12]
+        flux_aper_2 = vals[i][13]
+        flux_aper_3 = vals[i][14]
+        flux_aper_4 = vals[i][15]
+        flux_aper_5 = vals[i][16]
+        fluxerr_aper_1 = vals[i][17]
+        fluxerr_aper_2 = vals[i][18]
+        fluxerr_aper_3 = vals[i][19]
+        fluxerr_aper_4 = vals[i][20]
+        fluxerr_aper_5 = vals[i][21]
 
         ratio = str(float(awin_world) / float(bwin_world))
 
-        print(x_world,y_world,xwin_image,ywin_image,awin_world,bwin_world,awin_image,
-              bwin_image,fwhm_image,class_star,flux_aper_1,flux_aper_2,flux_aper_3,
-              flux_aper_4,flux_aper_5,fluxerr_aper_1,fluxerr_aper_2,fluxerr_aper_3,
-              fluxerr_aper_4,fluxerr_aper_5,ratio)
-
-        my_list = [x_world,y_world,xwin_image,ywin_image,awin_world,bwin_world,awin_image,
+        my_list = [number,flags,alphawin_j2000,deltawin_j2000,xwin_image,ywin_image,awin_world,bwin_world,awin_image,
                    bwin_image,fwhm_image,class_star,flux_aper_1,flux_aper_2,flux_aper_3,
                    flux_aper_4,flux_aper_5,fluxerr_aper_1,fluxerr_aper_2,fluxerr_aper_3,
                    fluxerr_aper_4,fluxerr_aper_5,ratio]
+
+        print(my_list)
 
         line = (", ").join(my_list)
         csvfile.write(f"{line}\n")
