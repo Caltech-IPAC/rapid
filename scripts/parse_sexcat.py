0d68385bf9cc6b3a14ecdf99f7f959efa9b7446d
Russ Laher <laher@ipac.caltech.edu>
2026-08-31 10:48:26 -0700

    Updated to parse the proposed columns from the SExtractor catalog for the XSources database table.

--- scripts/parse_sexcat.py
+++ scripts/parse_sexcat.py
@@ -1,11 +1,30 @@
 import numpy as np
 import modules.utils.rapid_pipeline_subs as util
 
-datafile="/Users/laher/Folks/rapid/awaicgen_output_mosaic_image_resampled_refgainmatchsexcat.txt";
-paramsfile="/Users/laher/git/rapid/cdf/rapidSexParamsGainMatch.inp";
+datafile="/Users/laher/Folks/rapid/sfftdiffimage_masked.txt";
+paramsfile="/Users/laher/git/rapid/cdf/rapidSexParamsDiffImage.inp";
 
-params_to_get = ["XWIN_IMAGE","YWIN_IMAGE","FLUX_APER_6","MAG_APER_6",
-                 "CLASS_STAR","ISOAREAF_IMAGE","AWIN_WORLD","BWIN_WORLD"]
+params_to_get = ["X_WORLD",
+                 "Y_WORLD",
+                 "XWIN_IMAGE",
+                 "YWIN_IMAGE",
+                 "AWIN_WORLD",
+                 "BWIN_WORLD",
+                 "AWIN_IMAGE",
+                 "BWIN_IMAGE",
+                 "FWHM_IMAGE",
+                 "CLASS_STAR",
+                 "FLUX_APER_1",
+                 "FLUX_APER_2",
+                 "FLUX_APER_3",
+                 "FLUX_APER_4",
+                 "FLUX_APER_5",
+                 "FLUXERR_APER_1",
+                 "FLUXERR_APER_2",
+                 "FLUXERR_APER_3",
+                 "FLUXERR_APER_4",
+                 "FLUXERR_APER_5",
+                ]
 
 vals = util.parse_ascii_text_sextractor_catalog(datafile,paramsfile,params_to_get)
 
@@ -21,21 +40,41 @@ with open('test_parse2.txt', 'w') as csvfile:
     csvfile.write(f"{line}\n")
 
     for i in range(num_rows):
-        xwin_image = vals[i][0]
-        ywin_image = vals[i][1]
-        flux_aper = vals[i][2]
-        mag_aper = vals[i][3]
-        class_star = float(vals[i][4])
-        isoareaf_image = float(vals[i][5])
-        awin_world = float(vals[i][6])
-        bwin_world = float(vals[i][7])
-        ratio = awin_world / bwin_world
 
-        print(xwin_image,ywin_image,flux_aper,mag_aper,class_star,isoareaf_image,awin_world,bwin_world,ratio)
+        x_world = vals[i][0]
+        y_world = vals[i][1]
+        xwin_image = vals[i][2]
+        ywin_image = vals[i][3]
+        awin_world = vals[i][4]
+        bwin_world = vals[i][5]
+        awin_image = vals[i][6]
+        bwin_image = vals[i][7]
+        fwhm_image = vals[i][8]
+        class_star = vals[i][9]
+        flux_aper_1 = vals[i][10]
+        flux_aper_2 = vals[i][11]
+        flux_aper_3 = vals[i][12]
+        flux_aper_4 = vals[i][13]
+        flux_aper_5 = vals[i][14]
+        fluxerr_aper_1 = vals[i][15]
+        fluxerr_aper_2 = vals[i][16]
+        fluxerr_aper_3 = vals[i][17]
+        fluxerr_aper_4 = vals[i][18]
+        fluxerr_aper_5 = vals[i][19]
 
-        line = xwin_image + ", " + ywin_image + ", " + flux_aper + ", " + \
-               mag_aper + ", " + str(class_star) + ", " + \
-               str(isoareaf_image) + ", " + str(awin_world) + ", " + str(bwin_world) + ", " + str(ratio)
+        ratio = str(float(awin_world) / float(bwin_world))
+
+        print(x_world,y_world,xwin_image,ywin_image,awin_world,bwin_world,awin_image,
+              bwin_image,fwhm_image,class_star,flux_aper_1,flux_aper_2,flux_aper_3,
+              flux_aper_4,flux_aper_5,fluxerr_aper_1,fluxerr_aper_2,fluxerr_aper_3,
+              fluxerr_aper_4,fluxerr_aper_5,ratio)
+
+        my_list = [x_world,y_world,xwin_image,ywin_image,awin_world,bwin_world,awin_image,
+                   bwin_image,fwhm_image,class_star,flux_aper_1,flux_aper_2,flux_aper_3,
+                   flux_aper_4,flux_aper_5,fluxerr_aper_1,fluxerr_aper_2,fluxerr_aper_3,
+                   fluxerr_aper_4,fluxerr_aper_5,ratio]
+
+        line = (", ").join(my_list)
         csvfile.write(f"{line}\n")
 
 
